b17a3097cd81fe324dedc9aa9efd0db0d21769c9
刘哲 <sking.pt@gmail.com>
2015-07-20 19:11:48 +0800

    update

--- test/entry_2.js
+++ test/entry_2.js
@@ -1,3 +1,3 @@
-define('comp/downloadtips', ['base/compbase', './test1', './test2'],
+define('comp/downloadtips_2', ['base/compbase', './test1', './test2'],
 function ($Base, $test1, $test2) {
 })
--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 "use strict";
 var opack = require('./lib/index');
 new opack({
-    entry: ['test/entry_2.js'],
+    entry: ['test/entry_1.js', 'test/entry_2.js'],
     nameRegx: 'comp/(.*)',
     outdir: 'dest'
 });
