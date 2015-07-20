a5ef365c7a6703e795c0411ddcc45efc38a75279
刘哲 <sking.pt@gmail.com>
2015-07-20 17:49:44 +0800

    update

--- test/test1.js
+++ test/test1.js
@@ -1,2 +1,6 @@
 define('test1', [], function () {
+    var test1 = {
+        testFun: 1
+    };
+    return test1
 }) ;
--- test/test2.js
+++ test/test2.js
@@ -1,2 +1,4 @@
 define('comp/download/test2', ['tpl/template'], function ($tpl) {
+    var rt = $tpl('c');
+    return rt;
 })
