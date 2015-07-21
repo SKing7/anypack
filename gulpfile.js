b0c28d449d88cbb72152787e8c7406d705052f30
刘哲 <sking.pt@gmail.com>
2015-07-21 16:40:25 +0800

    update test

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 //basic babel task
 var paths = {
     babel : 'src/*.js',
+    dest: 'test/lib',
+    testOutDest: 'test/dest',
 };
 var gulp = require("gulp");
+var fs = require("fs");
 var babel = require("gulp-babel");
 
+gulp.task('runtest', function() {
+    var pathOut = paths.testOutDest;
+    var exec = require('child_process').execSync;
+
+    if (!fs.existsSync(pathOut)) {
+        exec('mkdir ' + pathOut);
+    }
+    exec('gulp babel && node test/index', function (error, stdout, stderr) {
+        if (error) {
+            console.error(error);
+        }
+    });
+
+});
 gulp.task('babel', function() {
   return gulp.src(paths.babel)
   .pipe(babel())
-  .pipe(gulp.dest('test/src'))
+  .pipe(gulp.dest(paths.dest))
 })
 gulp.task('watch', ['babel'], function() {
   gulp.watch(paths.babel, ['babel'])
--- test/index.js
+++ test/index.js
@@ -3,5 +3,5 @@ var opack = require('./lib/index');
 new opack({
     entry: ['test/entry_1.js', 'test/entry_2.js'],
     nameRegx: 'comp/(.*)',
-    outdir: 'dest'
+    outdir: 'test/dest'
 });
